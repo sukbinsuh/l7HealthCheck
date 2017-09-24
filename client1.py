ec66de1c4c7430a5007c237d239198c1caf24423
suh <sukbin.suh@gmail.com>
2017-09-24 23:28:29 +0900

    - scheduler module삭제 - json request 주석 삽입

--- client1.py
+++ client1.py
@@ -1,33 +1,34 @@
 import requests
-import sched, time
+import time
 import os
 import subprocess
+import logging
 
 err_count = 0
 
 def check_health():
-    print "check Health"
+    logger.info('check_health() is excuted!')
     global  err_count
     r = None
     try:
         r = requests.get('http://localhost:8000', timeout=10)
     except requests.RequestException as err:
-        print err
+        logger.info(err)
         pass
     except requests.ConnectionError as err:
-        print err
+        logger.info(err)
         pass
     except requests.ReadTimeout as err:
-        print err
+        logger.info(err)
         pass
     except requests.URLRequired as err:
-        print err
+        logger.info(err)
         pass
     except requests.TooManyRedirects as err:
-        print err
+        logger.info(err)
         pass
     except requests.ConnectTimeout as err:
-        print err
+        logger.info(err)
         pass
     finally:
         if (r is not None and r.text == '"OK"'):
@@ -45,21 +46,37 @@ def kill_process(app_name):
 
 
 if __name__ == "__main__":
-    s = sched.scheduler(time.time, time.sleep)
+    """
+    params = dict(
+    origin='Chicago,IL',
+    destination='Los+Angeles,CA',
+    waypoints='Joplin,MO|Oklahoma+City,OK',
+    sensor='false'
+    )
+    
+    resp = requests.get(url=url, params=params)
+    data = json.loads(resp.text)
+    """
+    FORMAT = '%(asctime)-15s %(message)s'
+    logging.basicConfig(format=FORMAT, filename='client.log', level=logging.INFO)
+    logger = logging.getLogger('client')
+    logger.info('Start Metatron Health Check Client')
+
     while True:
         if err_count == 0:
-            print "err_count is 0"
-            s.enter(10, 5, check_health, ())
-            s.run()
+            logger.info('err_count is 0')
+            check_health()
+            time.sleep(10)
         elif err_count == 1:
-            print "err_count is 1"
-            s.enter(20, 5, check_health, ())
-            s.run()
+            logger.info('err_count is 1')
+            check_health()
+            time.sleep(20)
         elif err_count == 2:
-            print "err_count is 2"
-            event = s.enter(30, 5, check_health, ())
+            logger.info('err_count is 2')
+            check_health()
+            time.sleep(30)
         elif err_count == 3:
-            print "err_count is 3"
+            logger.info('err_count is 3')
             #kill metatron
             kill_process("Metatron")
             exit(1)
